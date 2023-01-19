f7446d35a31f32dac20eca03e743bcc65b7464ff
Tim <hshistudent@shacademy.org>
2023-01-19 14:24:10 -0800

    working on appstae

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:provider/provider.dart';
 
-void void main() {
-  runApp((MyApp));
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -23,25 +23,42 @@ return ChangeNotifierProvider(
 );
 }
 }
-class MyAppState extends ChangeNotifier {}
+class MyAppState extends ChangeNotifier {
+  var opacityList = [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0];
+}
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+var appState = context.watch<MyAppState>();
     return Scaffold(
       body: GridView.builder(
-        gridDelegate: SilverGridDelegateWithFixedCrossAxisCount(
+        itemCount: 9,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 3,
         ), 
         itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8),
-            child: Container(
-              height: 50,
-              width: 50,
-              color: Colors.blueGrey[700],
-              child: Image(image:Image.network('')),
-            ),
+          return Opacity(
+            opacity: 0.5,
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: SizedBox(
+                height: 50,
+                  width: 50,
+                child: Material(
+                  color: Colors.blueGrey[700],
+                  child: InkWell(
+                    child: Image.network(
+                      'https://i.giphy.com/media/xT0xezQGU5xCDJuCPe/200.gif'),
+                     onTap: () {},),
+                ),
+              ),
+              ),
           );
         },
 
